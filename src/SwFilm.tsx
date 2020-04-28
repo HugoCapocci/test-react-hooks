82516d48ad6d1cabc15463a0aab548be3cf52d69
Hugo CAPOCCI <h.capocci@gmail.com>
2020-04-28 19:24:32 +0200

    Add error handling

--- src/SwFilm.tsx
+++ src/SwFilm.tsx
@@ -2,20 +2,34 @@ import React from 'react';
 
 export function SwFilm({ id }: { id: number }) {
 
-    const [openingCrawl, setOpeningCrawl] = React.useState();
+    const [openingCrawl, setOpeningCrawl] = React.useState<string>();
+    const [error, setError] = React.useState<string>();
 
     React.useEffect(() => {
         const doFetch = async () => {
-            const response = await fetch(`https://swapi.dev/api/films/${id}/`);
-            const result = await response.json();
-            setOpeningCrawl(result['opening_crawl']);
+            try {
+                const response = await fetch(`https://swapi.dev/api/films/${id}/`);
+                if (response.status === 404) throw new Error('movie not found');
+                
+                const result = await response.json();
+                setOpeningCrawl(result['opening_crawl']);
+            } catch (error) {
+                setError(error.message);
+            }
         };
 
         doFetch();
     }, [id]);
 
-    return <div id="board">
-        <div id="content">{openingCrawl}</div>
+    return <div>
+        { !error &&
+            <div id="board">
+                <div id="content">{openingCrawl}</div>
+            </div>
+        }
+        { error && 
+            <div className="error">Error occurs: {error}</div>
+        }
     </div>;
 }
    
